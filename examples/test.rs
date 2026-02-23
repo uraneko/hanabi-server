c44675151f1bef52fb6542689cfabfa5e3ec4005
uraneko <BOOTHAlessandra@tutanota.com>
2026-02-23 09:38:50 +0100

    add sematic analysis

--- examples/test.rs
+++ examples/test.rs
@@ -1,4 +1,4 @@
-use hanabi_configs::{AnalyzeSyntax, Lex};
+use hanabi_configs::{AnalyzeSemantics, AnalyzeSyntax, Lex};
 
 const INPUT: &[u8; 103] =
     b"[asd.we]\n[t43r\\] ca.43we\\.e3]\r\n [ \\...\\]  \\.  \\]..]    \n  #fqe#t4wrv\r\nfavdrd=rh\\=fdvs\n\r4hewds\\=er#vsc./";
@@ -6,7 +6,12 @@ fn main() {
     let mut lex = Lex::new(INPUT);
     let tokens = lex.lex().unwrap();
     println!("{:?}", tokens);
+
     let analyze = AnalyzeSyntax::new(tokens);
     let groups = analyze.analyze().unwrap();
     println!("{:?}", groups);
+
+    let analyze = AnalyzeSemantics::new(groups);
+    let components = analyze.analyze().unwrap();
+    println!("{:?}", components);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
+mod analyze_semantics;
 mod analyze_syntax;
 mod lex;
 
+pub use analyze_semantics::AnalyzeSemantics;
 pub use analyze_syntax::AnalyzeSyntax;
 pub use lex::Lex;
-
