9f55aac655dd1bd6c2c29fe1b41d04dcad28c7f7
uraneko <BOOTHAlessandra@tutanota.com>
2026-02-23 08:09:35 +0100

    add syntax analysis

--- examples/test.rs
+++ examples/test.rs
@@ -1,9 +1,12 @@
-use hanabi_configs::Lex;
+use hanabi_configs::{AnalyzeSyntax, Lex};
 
 const INPUT: &[u8; 103] =
     b"[asd.we]\n[t43r\\] ca.43we\\.e3]\r\n [ \\...\\]  \\.  \\]..]    \n  #fqe#t4wrv\r\nfavdrd=rh\\=fdvs\n\r4hewds\\=er#vsc./";
 fn main() {
     let mut lex = Lex::new(INPUT);
-    let tokens = lex.lex();
+    let tokens = lex.lex().unwrap();
     println!("{:?}", tokens);
+    let analyze = AnalyzeSyntax::new(tokens);
+    let groups = analyze.analyze().unwrap();
+    println!("{:?}", groups);
 }
